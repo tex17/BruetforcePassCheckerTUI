0676472377acdf017abd2eced31e20736c1ec0a1
Tex Shelest <tex17@yandex.ru>
2024-03-26 14:57:50 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace BruetforcePassCheckerTUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Application.Init();
+            try
+            {
+                Application.Run(new MyView());
+            }
+            finally
+            {
+                Application.Shutdown();
+            }
+        }
+    }
+}
